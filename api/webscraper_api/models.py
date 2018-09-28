e349592c8cae0340dfbd22cdd1fcb7a1786ef6ca
MarkPassando <markpassando@gmail.com>
2018-09-27 20:48:08 -0400

    Create django admin view

--- api/webscraper_api/models.py
+++ api/webscraper_api/models.py
@@ -16,18 +16,19 @@ class UserManager(BaseUserManager):
 
     return user
 
-    def create_superuser(self, email, name, password):
-      user = self.create_user(email, name, password)
-      user.is_superuser = True
+  def create_superuser(self, email, name, password):
+    user = self.create_user(email, name, password)
+    user.is_superuser = True
+    user.is_staff = True
+    user.save(using=self._db)
 
-      user.save(using=self._db)
-
-      return user
+    return user
 
 class User(AbstractBaseUser, PermissionsMixin):
   email = models.EmailField(max_length=255, unique=True)
   name = models.CharField(max_length=255)
   is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
 
   objects = UserManager()
 
